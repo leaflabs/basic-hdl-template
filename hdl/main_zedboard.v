e331a4109d1e5de4491c3e9acb9453651230e922
bryan newbold <bnewbold@leaflabs.com>
2013-11-06 13:09:49 -0500

    initial zed work: fix main module to match .ucf naming of internal (PS-PL) nets

--- hdl/main_zedboard.v
+++ hdl/main_zedboard.v
@@ -11,40 +11,39 @@
 `timescale 1 ps / 1 ps
 
 module main (
-    // TODO: are these PS_* inputs or outputs? or what?
-    /*
-    inout wire PS_PORB,
-    inout wire PS_SRSTB,
-    inout wire PS_CLK,
-    */
-    inout wire [15:0] DDR_addr,
-    inout wire [2:0] DDR_ba,
-    inout wire DDR_cas_n,
-    inout wire DDR_ck_n,
-    inout wire DDR_ck_p,
-    inout wire DDR_cke,
-    inout wire DDR_cs_n,
-    inout wire [3:0] DDR_dm,
-    inout wire [31:0] DDR_dq,
-    inout wire [3:0] DDR_dqs_n,
-    inout wire [3:0] DDR_dqs_p,
-    inout wire DDR_odt,
-    inout wire DDR_ras_n,
-    inout wire DDR_reset_n,
-    inout wire DDR_we_n,
-    inout wire FIXED_IO_ddr_vrn,
-    inout wire FIXED_IO_ddr_vrp,
-    inout wire [53:0] FIXED_IO_mio,
-    inout wire FIXED_IO_ps_clk,
-    inout wire FIXED_IO_ps_porb,
-    inout wire FIXED_IO_ps_srstb,
+    // Buttons, LEDs, Switches
     output wire [7:0] leds,
+    input wire [7:0] switches,
     input wire button_center,
     input wire button_down,
     input wire button_left,
     input wire button_right,
     input wire button_up,
-    input wire [7:0] switches
+
+    // Internal (PS-PL) DDR Interface
+    inout wire [15:0] DDR_Addr,
+    inout wire [2:0] DDR_BankAddr,
+    inout wire DDR_CAS_n,
+    inout wire DDR_Clk_n,        // was: DDR_ck_n
+    inout wire DDR_Clk_p,        // was: DDR_ck_p
+    inout wire DDR_CKE,
+    inout wire DDR_CS_n,
+    inout wire [3:0] DDR_DM,
+    inout wire [31:0] DDR_DQ,
+    inout wire [3:0] DDR_DQS_n,
+    inout wire [3:0] DDR_DQS_p,
+    inout wire DDR_ODT,
+    inout wire DDR_RAS_n,
+    inout wire DDR_DRSTB,       // was: DDR_reset_n
+    inout wire DDR_WEB,         // was: DDR_we_n
+    inout wire DDR_VRN,
+    inout wire DDR_VRP,
+
+    // Internal (PS-PL) I/O
+    inout wire [53:0] MIO,
+    inout wire PS_PORB,
+    inout wire PS_SRSTB,
+    inout wire PS_CLK
     );
 
     wire axi_aclk;
@@ -71,27 +70,27 @@ module main (
     wire [0:0] axi_slave1_wvalid;
 
 block_design block_design_i
-       (.DDR_addr(DDR_addr),
-        .DDR_ba(DDR_ba),
-        .DDR_cas_n(DDR_cas_n),
-        .DDR_ck_n(DDR_ck_n),
-        .DDR_ck_p(DDR_ck_p),
-        .DDR_cke(DDR_cke),
-        .DDR_cs_n(DDR_cs_n),
-        .DDR_dm(DDR_dm),
-        .DDR_dq(DDR_dq),
-        .DDR_dqs_n(DDR_dqs_n),
-        .DDR_dqs_p(DDR_dqs_p),
-        .DDR_odt(DDR_odt),
-        .DDR_ras_n(DDR_ras_n),
-        .DDR_reset_n(DDR_reset_n),
-        .DDR_we_n(DDR_we_n),
-        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
-        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
-        .FIXED_IO_mio(FIXED_IO_mio),
-        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
-        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
-        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
+       (.DDR_addr(DDR_Addr),
+        .DDR_ba(DDR_BankAddr),
+        .DDR_cas_n(DDR_CAS_n),
+        .DDR_ck_n(DDR_Clk_n),
+        .DDR_ck_p(DDR_Clk_p),
+        .DDR_cke(DDR_CKE),
+        .DDR_cs_n(DDR_CS_n),
+        .DDR_dm(DDR_DM),
+        .DDR_dq(DDR_DQ),
+        .DDR_dqs_n(DDR_DQS_n),
+        .DDR_dqs_p(DDR_DQS_p),
+        .DDR_odt(DDR_ODT),
+        .DDR_ras_n(DDR_RAS_n),
+        .DDR_reset_n(DDR_DRSTB),
+        .DDR_we_n(DDR_WEB),
+        .FIXED_IO_ddr_vrn(DDR_VRN),
+        .FIXED_IO_ddr_vrp(DDR_VRP),
+        .FIXED_IO_mio(MIO),
+        .FIXED_IO_ps_clk(PS_CLK),
+        .FIXED_IO_ps_porb(PS_PORB),
+        .FIXED_IO_ps_srstb(PS_SRSTB),
         .axi_aclk(axi_aclk),
         .axi_aresetn(axi_aresetn),
         .axi_gpio_buttons_tri_i({button_center,
