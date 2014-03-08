bba4b38946f0615990c170f09b979adc4f6de1be
bryan newbold <bnewbold@leaflabs.com>
2014-03-07 19:00:51 -0500

    zynq: use /mnt for bitfile storage
    This fixes a problem with current zedboard buildroot images whereby the
    bitfile is too large for ramfs storage.

--- contrib/zed/zynq_targets.mk
+++ contrib/zed/zynq_targets.mk
@@ -7,16 +7,16 @@ zynq_ssh_host ?= zedboard
 bitfile_list += build/$(project).bit.bin
 
 scp: build/$(project).bit.bin
-	scp build/$(project).bit.bin $(zynq_ssh_host):
+	scp build/$(project).bit.bin $(zynq_ssh_host):/mnt
 
 prog_ssh:
 	@# Fail if build/$(project).bit.bin not built yet
 	@[ -f build/$(project).bit.bin ] || false
-	scp build/$(project).bit.bin $(zynq_ssh_host):
+	scp build/$(project).bit.bin $(zynq_ssh_host):/mnt
 	@echo "Creating /dev/xdevcfg if it doesn't exist already..."
 	ssh $(zynq_ssh_host) -- "[ -c /dev/xdevcfg ] || mknod /dev/xdevcfg c 259 0"
 	@echo "Loading bitfile into PL..."
-	ssh $(zynq_ssh_host) -- "cat $(project).bit.bin > /dev/xdevcfg"
+	ssh $(zynq_ssh_host) -- "cat /mnt/$(project).bit.bin > /dev/xdevcfg"
 
 build/$(project).bit.bin: build/$(project).bit
 	@bash -c "$(xil_env); promgen -b -p bin -data_width 32 -u 0x0 $(project).bit -w -o $(project).bit.bin"
